5f2b1ac7765196a61f1430d6ba3a634d5e2ea95c
Musaddik Hossain <musaddikh13email.com>
2025-11-04 14:24:29 +0600

    fix version

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 export const axiosi = axios.create({
     withCredentials: true,
-    baseURL: process.env.REACT_APP_BASE_URL || 'http://localhost:8000/api/',
+    baseURL: process.env.REACT_APP_BASE_URL || 'https://hot-wheels-shop-backend.vercel.app/api/',
   });
   
